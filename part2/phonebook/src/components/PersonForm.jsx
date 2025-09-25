ea4a6c60a03bf50150cd0efa7e45a116acfde511
Felix <160373484+jflxx@users.noreply.github.com>
2025-09-25 12:03:40 -0500

    phonebook step 10

--- part2/phonebook/src/components/PersonForm.jsx
+++ part2/phonebook/src/components/PersonForm.jsx
@@ -14,7 +14,7 @@ const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, persons, set
     const handleOnSubmit = (event) => {
         event.preventDefault()
 
-        const exist = persons.some(person => person.name === newName || person.number === newNumber);
+        const exist = persons.some(person => person.name === newName);
         if (!exist) {
             const nameObject = {
                 name: newName,
@@ -27,7 +27,15 @@ const PersonForm = ({ newName, setNewName, newNumber, setNewNumber, persons, set
             })
 
         } else {
-            alert(`${newName} is already added to phonebook`);
+                if(window.confirm(`${newName} is already added to phonebook, replace the old Number with the new one`)){
+                    const person = persons.find(person => person.name === newName);
+                    const changedPerson = {...person, number: newNumber};
+                    personsService.update(person.id,changedPerson).then(returnedPerson =>{
+                        setPersons(persons.map(p => p.id !== person.id? p: returnedPerson));
+                        setNewName('');
+                        setNewNumber('');
+                    })
+                }
         }
     }
 
